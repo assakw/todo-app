a0fcc71d53ac5ccdaf86c4a2ef84335d7d7bc218
kkw <assakw@naver.com>
2021-09-02 08:54:33 +0900

    new commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+
+const App = () => {
+  return <TodoTemplate>Todo App Make</TodoTemplate>;
+};
+
+export default App;
