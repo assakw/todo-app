bf06fb671c47155f2234ba0dd6638b6f36671755
kkw <assakw@naver.com>
2021-10-01 09:01:41 +0900

    [todo app] 렌더링 속도 개선

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import {MdAdd} from 'react-icons/md';
+import React, { useCallback, useState } from 'react';
+import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
-    return (
-        <form className="TodoInsert">
-            <input placeholder="TodoInsert" />
-            <button type="submit">
-                <MdAdd />
-            </button>
-        </form>
-    );
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      //submit 새로고침 방지
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
+  // button에 사용가능 onSubmit 엔터 작동됨, onClick enter 작동 코드 추가 필요
+  // const onClick = useCallback(() => {
+  //   onInsert(value);
+  //   setValue('');
+  // }, [onInsert, value]);
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
+
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
 };
 
 export default TodoInsert;
